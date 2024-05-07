c55d1dd9e59272072f98907106fc04f7d4cd618c
chl8469 <hl8469@gmail.com>
2024-05-07 23:57:08 +0900

    orm 및 mapper 추가 #1

--- cosmic_python_with_fastapi/orm.py
+++ cosmic_python_with_fastapi/orm.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Column, Integer, String, Table
-from sqlalchemy.orm import registry
+from sqlalchemy import Column, Date, ForeignKey, Integer, String, Table
+from sqlalchemy.orm import registry, relationship
 
-from model import OrderLine
+import model
 
 mapper_registry = registry()
 
@@ -15,6 +15,36 @@ order_lines = Table(
     Column("orderid", String(255)),
 )
 
+batches = Table(
+    "batches",
+    mapper_registry.metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("reference", String(255)),
+    Column("sku", String(255)),
+    Column("_purchased_quantity", Integer, nullable=False),
+    Column("eta", Date, nullable=True),
+)
+
+allocations = Table(
+    "allocations",
+    mapper_registry.metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("orderline_id", ForeignKey("order_lines.id")),
+    Column("batch_id", ForeignKey("batches.id")),
+)
+
 
 def start_mappers():
-    mapper_registry.map_imperatively(OrderLine, order_lines)
+    mapper_registry.map_imperatively(model.OrderLine, order_lines)
+    mapper_registry.map_imperatively(
+        model.Batch,
+        batches,
+        properties={
+            "_allocations": relationship(
+                model.OrderLine,
+                secondary=allocations,
+                collection_class=set,
+                lazy="joined",
+            ),
+        },
+    )
