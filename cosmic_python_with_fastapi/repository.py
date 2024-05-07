905fee56f1dde748854ba21f6d1882741af1b2ff
chl8469 <hl8469@gmail.com>
2024-05-07 23:58:25 +0900

    sqlalchemy repository 구현

--- cosmic_python_with_fastapi/repository.py
+++ cosmic_python_with_fastapi/repository.py
@@ -1,5 +1,7 @@
 from typing import Protocol
 
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 import model
 
@@ -9,3 +11,15 @@ class RepositoryProtocol(Protocol):
 
     def get(self, reference: str) -> model.Batch: ...
 
+
+class SqlAlchemyRepository:
+    def __init__(self, session: AsyncSession) -> None:
+        self.session = session
+
+    def add(self, batch: model.Batch) -> None:
+        self.session.add(batch)
+
+    async def get(self, reference: str) -> model.Batch:
+        stmt = select(model.Batch).where(model.Batch.reference == reference)
+        result = await self.session.execute(stmt)
+        return result.unique().scalars().one()
